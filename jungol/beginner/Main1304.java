bda6d20ca787ff8d45f87b3c86ab778ea44d0854
Bora Jin <realb8179@gmail.com>
2021-03-16 21:52:48 +0900

    20210315_jungol1304 -> 풀이추가

--- jungol/beginner/Main1304.java
+++ jungol/beginner/Main1304.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main1304 {
     public static void main(String args[]) {
+        //20210315
 
-        //풀이1
+        //풀이1 : 바깥 반복문은 열, 안쪽 반복문은 행임. 문제는 행과 열이 뒤집혀있으니 배열에 저장할 때 인덱스를 반대로 바꿔주면 됨.
         int i, j, length, num=1;
         int[][] arr = new int[100][100];
 
@@ -24,7 +25,7 @@ public class Main1304 {
             System.out.println("");
         }
 
-        //풀이2
+        //풀이2 : 행 사이 수와 열이 바뀔 때의 규칙을 찾음.
         /*
         int i, j, num=1, length;
 
